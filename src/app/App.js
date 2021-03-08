41dd6a00fae2e855bb86f58ee7e5e6d626ff540e
Vatsal <vatsalpatel1073@gmail.com>
2021-03-08 19:37:56 +0530

    finished the custom div

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import React, { Suspense } from "react";
+import styles from "./App.scss";
+import Div from "./common/components/div";
+
+function App() {
+  return (
+    <div className='app'>
+      <Div />
+    </div>
+  );
+}
+
+export default App;
