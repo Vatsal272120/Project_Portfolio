a1441cdb7ae9b3146922a4b83a233f1c8063892b
Vatsal <vatsalpatel1073@gmail.com>
2021-03-09 10:16:45 +0530

    pagination Button

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,13 @@
 import React, { Suspense } from "react";
 import styles from "./App.scss";
 import Div from "./common/components/div";
+import PaginationButton from "./common/components/paginationButton";
 
 function App() {
   return (
     <div className='app'>
       <Div />
+      <PaginationButton />
     </div>
   );
 }
--- src/app/common/components/paginationButton/index.js
+++ src/app/common/components/paginationButton/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import styles from "./pagination_button.module.scss";
 import leftArrow from "../../../assets/icons/icon-left-arrow.png";
+import Div from "../div/index";
 
-const PaginationButton = () => {
-  return <div></div>;
+const PaginationButton = ({ className, isEnabled, onClick, isRight }) => {
+  return (
+    <Div
+      align
+      justify
+      className={`${styles.button_container} ${className} ${
+        !isEnabled ? styles.disabled : ""
+      }`}
+      onClick={onClick}></Div>
+  );
 };
 
 export default PaginationButton;
