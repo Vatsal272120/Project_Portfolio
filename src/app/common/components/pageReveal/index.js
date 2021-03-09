99ffea8cf338f7c38954a04c681c3161d36017f5
Vatsal <vatsalpatel1073@gmail.com>
2021-03-09 10:30:21 +0530

    pageReveal

---
+++ src/app/common/components/pageReveal/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./pageReveal.scss";
+
+const PageReveal = ({ animate }) => {
+  return (
+    <div className={styles["revealer"]}>
+      <div
+        className={`${styles["revealer-layer"]} ${
+          animate ? styles["animate-layer"] : ""
+        }`}
+      />
+      <div
+        className={`${styles["revealer-layer"]} ${
+          animate ? styles["animate-layer"] : ""
+        }`}
+      />
+    </div>
+  );
+};
+
+export default PageReveal;
