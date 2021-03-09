00097a3b7900fbcb1b3bce87e4b6bbbfd159122f
Vatsal <vatsalpatel1073@gmail.com>
2021-03-09 10:11:22 +0530

    assets added

---
+++ src/app/common/components/paginationButton/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./pagination_button.module.scss";
+import leftArrow from "../../../assets/icons/icon-left-arrow.png";
+
+const PaginationButton = () => {
+  return <div></div>;
+};
+
+export default PaginationButton;
